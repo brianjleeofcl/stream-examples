8b77daf37527da1435956cdcdcf9184a6915f096
Brian Lee <brianjleeofcl@gmail.com>
2017-05-28 10:15:58 -0700

    Heroku preparation

---
+++ server/server.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var app = express();
+app.post('/upload-image', function (req, res, next) {
+    if (!req.is('image/*'))
+        return next(new Error('Unacceptable Content Type'));
+    var buffers = [];
+    req.on('data', function (chunk) { return buffers.push(chunk); }).on('end', function () {
+        res.send('Image accepted');
+    });
+});
+var port = process.env.port || 8000;
+app.listen(port, function () {
+    console.log("listening on port " + port);
+});
+exports["default"] = app;
